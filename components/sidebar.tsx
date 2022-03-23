7c487c338143d7a282664daac54eb0b3e2f42c10
Jeffx234 <jefersonluisx@hotmail.com>
2022-03-23 13:27:40 -0300

    Modelagem de dados com Prisma

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -70,7 +70,7 @@ const Sidebar = () => {
             {navMenu.map((menu) => (
               <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
                 <LinkBox>
-                  <NextLink href={menu.route} passHeref>
+                  <NextLink href={menu.route} passHref>
                     <LinkOverlay>
                       <ListIcon
                         as={menu.icon}
